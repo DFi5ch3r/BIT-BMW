748709b9a78193991ef76ada863a0a65e045aa5c
d.fischer@tu-berlin.de <d.fischer@tu-berlin.de>
2024-09-12 15:12:46 +0000

    Edited theme/parameters.yaml, analysis.ItemTemplate1.ItemTemplate2, analysis and analysis.ItemTemplate1

--- client_code/analysis/ItemTemplate1/__init__.py
+++ client_code/analysis/ItemTemplate1/__init__.py
@@ -7,12 +7,12 @@ class ItemTemplate1(ItemTemplate1Template):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
-    self.check_box_1.text = self.item['Baureihe']
+    self.check_box_type.text = self.item['Baureihe']
     self.repeating_panel_1.items = self.item['Years']
     # Any code you write here will run before the form opens.
 
-  def button_1_click(self, **event_args):
+  def button_expandYears_click(self, **event_args):
     self.repeating_panel_1.visible = not(self.repeating_panel_1.visible)
 
-  def check_box_1_change(self, **event_args):
+  def check_box_type_change(self, **event_args):
     self.repeating_panel_1.raise_event_on_children('x-toggleBox')
