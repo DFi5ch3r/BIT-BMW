abe5f229fa2ee8d1079baa247ed84cacd74ba5c3
d.fischer@tu-berlin.de <d.fischer@tu-berlin.de>
2024-09-16 10:41:00 +0000

    Edited IO, analysis, analysis.ItemTemplate1.ItemTemplate2, globals and 1 more

--- client_code/analysis/ItemTemplate1/ItemTemplate2/__init__.py
+++ client_code/analysis/ItemTemplate1/ItemTemplate2/__init__.py
@@ -7,9 +7,14 @@ class ItemTemplate2(ItemTemplate2Template):
   def __init__(self, **properties):
 
     self.init_components(**properties)
-    self.check_box_years.text = self.item
+    self.check_box_years.text = self.item['year']
+    
     self.set_event_handler('x-toggleBox', self.toggleBox)
 
   def toggleBox(self, **event_args):
     self.check_box_years.checked=event_args['check']
+
+  def check_box_years_change(self, **event_args):
+    """This method is called when this checkbox is checked or unchecked"""
+    pass
  
